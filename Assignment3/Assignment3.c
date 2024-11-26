6061c974733aeda7d92bd9b03f164904ba9efd24
Joshorsley <146381020+Joshorsley@users.noreply.github.com>
2024-11-26 16:05:58 -0500

    Add project files.

---
+++ Assignment3/Assignment3.c
@@ -0,0 +1,29 @@
+#include <mysql.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	const char* server = "localhost";
+	const char* username = "root";
+	const char* password = "root";
+	const char* defaultDatabase = "sakila";
+
+	// step 1. Initialize the MySQL Object
+	MYSQL* databaseObject = mysql_init(NULL);
+
+	if (databaseObject == NULL) 
+	{
+		printf("Error Initializing the Database Object\n");
+		return EXIT_FAILURE;
+	}
+
+	// step 2. Connect to the MySQL Server
+	if (!mysql_real_connect(databaseObject, server, username, password, defaultDatabase, 3306, NULL, 0))
+	{
+		printf("Error Connecting to the MySQL Server\n");
+		return EXIT_FAILURE;
+	}
+
+
+}
